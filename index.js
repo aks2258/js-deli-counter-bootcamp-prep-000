feef7ab20a4557f9637488de10c5621a922aae24
Arnie Serrano <arniekidd@gmail.com>
2020-06-15 16:47:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,19 +16,16 @@ function nowServing(katzDeliLine){
   }
 }
 
-function currentLine(list) {
-  if (list.length === 0) {
-    return "The line is currently empty."
-  } else {
-    var str = "The line is currently: "
-    let place = 1
-    let index = 0
-    while (index < list.length) {
-      str += `${place}. ${list[index]}, `
-      place++
-      index++
-      }
-      str = str.slice(0, -2)
-    }
-    return str
+function currentLine(katzDeliline) {
+  if (katzDeliline.length ==0 0) {
+   return 'The line is currently empty.'
+  } else if (katzDeliline.length > 0) {
+    var array = []
+    for (var i = 0; i < katzDeliline.length; i++) {
+    
+    array.push(`${i + 1}` + '. ' + `${katzDeliline[i]}`)
   }
+    return 'The line is currently: ' + `${array.join(', ')}`
+  } 
+  
+}

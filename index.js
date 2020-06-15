0480618cff564fba321b57771dd1a435f048847c
Arnie Serrano <arniekidd@gmail.com>
2020-06-15 16:43:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,14 +16,16 @@ function nowServing(katzDeliLine){
   }
 }
 
-function currentLine(line) {
-  if (!line.length) {
-    return 'The line is currently empty.';
+function currentLine(katzDeliline) {
+  if (katzDeliline.length == 0) {
+   return 'The line is currently empty.'
+  } else if (katzDeliline.length > 0) {
+    var array = []
+    for (var i = 0; i < katzDeliline.length; i++) {
+    
+    array.push(`${i + 1}` + '. ' + `${katzDeliline[i]}`)
   }
-
-  let numbersAndNames = [];
-  for (let i = 0; i < line.length; i++) {
-    numbersAndNames.push(`${i + 1}. ${line[i]}`);
-  }
-  return 'The line is currently: ' + numbersAndNames.join(', ');
+    return 'The line is currently: ' + `${array.join(', ')}`
+  } 
+  
 }

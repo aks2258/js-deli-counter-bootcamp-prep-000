daa7ea8dc162bcb52b9b7439a6729b93deb3413c
Arnie Serrano <arniekidd@gmail.com>
2020-06-15 16:42:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,19 +16,14 @@ function nowServing(katzDeliLine){
   }
 }
 
-function currentLine(list) {
-  if (list.length === 0) {
-    return "The line is currently empty."
-  } else {
-    var str = "The line is currently: "
-    let place = 1
-    let index = 0
-    while (index < list.length) {
-      str += `${place}. ${list[index]}, `
-      place++
-      index++
-      }
-      str = str.slice(0, -2)
-    }
-    return str
+function currentLine(line) {
+  if (!line.length) {
+    return 'The line is currently empty.';
   }
+
+  let numbersAndNames = [];
+  for (let i = 0; i < line.length; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`);
+  }
+  return 'The line is currently: ' + numbersAndNames.join(', ');
+}

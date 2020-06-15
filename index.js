1540cf6957c48a5bbdf55fa8f0962534a7edd09f
Arnie Serrano <arniekidd@gmail.com>
2020-06-15 16:40:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,19 +16,16 @@ function nowServing(katzDeliLine){
   }
 }
 
-function currentLine(list) {
-  if (list.length === 0) {
-    return "The line is currently empty."
-  } else {
-    var str = "The line is currently: "
-    let place = 1
-    let index = 0
-    while (index < list.length) {
-      str += `${place}. ${list[index]}, `
-      place++
-      index++
+function currentLine(katzDeliLine) {
+  var line = "The line is currently: "
+  var arr = []
+  if (katzDeliLine.length >0){
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      arr.push((i+1) + ". " + katzDeliLine[i])
       }
-      str = str.slice(0, -2)
+    return line + arr.join(", ")
     }
-    return str
+  else {
+    return "The line is currently empty."
   }
+}
